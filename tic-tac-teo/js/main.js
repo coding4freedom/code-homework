b3a363bf5a10407b0841ef06fa766afe7ab82a74
coding4freedom <chawkins2010@hotmail.com>
2022-05-31 23:46:39 -0500

    Check who goes first

--- tic-tac-teo/js/main.js
+++ tic-tac-teo/js/main.js
@@ -1,3 +1,6 @@
+// target selector
+let turn = document.querySelector('.turn');
+
 // logic for which player goes first
 function goFirst(){
     let flip = Math.floor(Math.random() * 30);
@@ -5,6 +8,21 @@ function goFirst(){
 }
 // goFirst function check
 // console.log(goFirst)
+function checkTurn(func){
+    let first;
+    let random = func;
+    if(random === 'player'){
+        first = 'player';
+        turn.innerText = 'Player 1 goes first, using X!' 
+    }
+    else if(random === 'bot'){
+        first = 'bot';
+        turn.innerText = 'Computer will go first, using O!'
+    }
+    // console.log(first)
+    return first;
+}
+checkTurn(goFirst());
 
 // display to player who turn it is
 // create logic to switch between player and bot
